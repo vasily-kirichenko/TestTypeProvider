f633d9b072a05dc82f793af9dc32abcb89c8a66b
vasily-kirichenko <vaskir@gmail.com>
2013-07-15 20:35:05 +0400

    add a comment

--- TestTypeProvider/TestScript.fsx
+++ TestTypeProvider/TestScript.fsx
@@ -2,5 +2,11 @@
 
 type T = Samples.ShareInfo.TPTest.TPTestType
 let t = T()
+
+// OK
 let my1 = t.createMydirectly()
+
+// Error: The type provider 'Samples.FSharp.TestTypeProvider.TypeProvider1' reported an error 
+// in the context of provided type 'Samples.ShareInfo.TPTest.TPTestType', member 'returnInstanceOfMy'. 
+// The error: Unsupported constant type 'Samples.FSharp.TestTypeProvider.My'	
 let my2 = t.returnInstanceOfMy()
