7c61f552ae3a63e9559fbf18dfb1bfdcd4160a01
vasily-kirichenko <vaskir@gmail.com>
2013-07-15 20:31:11 +0400

    initial load

---
+++ TestTypeProvider/TestScript.fsx
@@ -0,0 +1,6 @@
+﻿#r @".\bin\Debug\TestTypeProvider.dll"
+
+type T = Samples.ShareInfo.TPTest.TPTestType
+let t = T()
+let my1 = t.createMydirectly()
+let my2 = t.returnInstanceOfMy()
---
+++ TestTypeProvider/TypeProvider1.fs
@@ -0,0 +1,37 @@
+﻿namespace Samples.FSharp.TestTypeProvider
+
+open System.Reflection
+open Microsoft.FSharp.Core.CompilerServices
+open Samples.FSharp.ProvidedTypes
+
+type My = { P: int }
+
+[<TypeProvider>]
+type public TypeProvider1() as this =
+    inherit TypeProviderForNamespaces()
+    let thisAssembly = Assembly.GetExecutingAssembly()
+    let rootNamespace = "Samples.ShareInfo.TPTest"
+    let baseTy = typeof<obj>
+    let newT = ProvidedTypeDefinition(thisAssembly, rootNamespace, "TPTestType", Some baseTy)
+    
+    do newT.AddMember(
+        ProvidedMethod(
+                "createMydirectly",
+                [],
+                typeof<My>,
+                InvokeCode = fun args -> <@@ { P = 2 } @@>))
+
+    let my = { P = 3 }
+
+    do newT.AddMember(
+        ProvidedMethod(
+                "returnInstanceOfMy",
+                [],
+                typeof<My>,
+                InvokeCode = fun args -> <@@ my @@>))
+
+    do newT.AddMember (ProvidedConstructor([], InvokeCode = fun args -> <@@ () @@>)) 
+    do this.AddNamespace (rootNamespace, [newT])
+
+[<TypeProviderAssembly>]
+do ()
